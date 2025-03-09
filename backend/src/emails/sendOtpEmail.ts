f12555fd7b5c0f95b914d75c226cc032195ce408
azaadmottan <azaadmottan2022@gmail.com>
2025-03-09 19:21:27 +0530

    Setup email service for send otp to the user

---
+++ backend/src/emails/sendOtpEmail.ts
@@ -0,0 +1,70 @@
+import nodemailer from "nodemailer";
+import { config } from "../config/config.js";
+
+const sendOTPEmail = async ( userName: string, email: string, otp: string): Promise<boolean> => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      secure: true,
+      port: 465,
+      auth: { 
+        user: config.otpEmailAddress, 
+        pass: config.otpEmailPassword, 
+      },
+    });
+  
+    const emailHtml = `
+      <html>
+        <head>
+          <style>
+            body { 
+              font-family: Arial, sans-serif; 
+              background-color: #f4f4f4; }
+            .container { 
+              max-width: 600px; 
+              margin: 20px auto; 
+              background: #ffffff; 
+              padding: 20px; 
+              border-radius: 8px; 
+              text-align: left; }
+            .otp { 
+              font-size: 24px; 
+              font-weight: bold; 
+              color: #007bff; 
+              background: #e9ecef; 
+              padding: 10px 20px; 
+              border-radius: 6px; 
+              display: inline-block; }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h2>OTP Verification</h2>
+            <p>Hello <strong>${userName}</strong>,</p>
+            <p>Your One-Time Password (OTP) for email verification is:</p>
+            <div class="otp">${otp}</div>
+            <p>This OTP is valid for next <strong>10 minutes</strong>. Please do not share it with anyone.</p>
+            <p>If you did not request this, please ignore this email.</p>
+            <p class="best-regards">Best Regards,</p>
+            <p class="link-sphere"><strong>LinkSphere</strong></p>
+            <p style="font-size: 12px; color: #888;">&copy; 2025 LinkSphere. All Rights Reserved.</p>
+          </div>
+        </body>
+      </html>
+    `;
+  
+    const response = await transporter.sendMail({
+      from: `LinkSphere <linksphere@socialmedia.in>`,
+      to: email,
+      subject: "Signup Verification Code",
+      html: emailHtml,
+    });
+
+    return true;
+  } catch (error) {
+    console.error(`Error while send otp: ${error}`);
+    return false;
+  }
+};
+
+export { sendOTPEmail };
