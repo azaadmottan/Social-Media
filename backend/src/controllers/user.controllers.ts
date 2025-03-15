970748f777efe15b7a7baee2880779ff151cd0b8
azaadmottan <azaadmottan2022@gmail.com>
2025-03-15 13:56:13 +0530

    Create and implement user controller: verifyUserAccount, resendOtp, loginUser, logoutUser, updatePassword, updateToken, updateUserRole, getCurrentUser, updateUserAccountDetails, getUserById, getAllUsers, getAccountActivationCode, verifyAccountActivationCode, deactivateAccount, deleteAccount & setup sendAccountActivationCode email

--- backend/src/controllers/user.controllers.ts
+++ backend/src/controllers/user.controllers.ts
@@ -1,11 +1,26 @@
+import { Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { ApiError } from "../utils/apiError.js";
+import { config } from "../config/config.js";
 import { IAuthenticatedRequest, IUser } from "../interfaces/user.interface.js";
-import { Response } from "express";
 import { UserModel } from "../models/user.model.js";
-import { userSchema } from "../schemas/user.schema.js";
-import { sendOTPEmail } from "../emails/sendOtpEmail.js";
+import { sendAccountActivationCode, sendEmailVerificationCode } from "../emails/sendOtpEmail.js";
+import { 
+  emailValidationSchema,
+  getAllUsersSchema,
+  getUserByIdSchema,
+  loginSchema, 
+  verifyOtpSchema, 
+  refreshTokenSchema, 
+  updatePasswordSchema, 
+  updateUserProfileSchema, 
+  userRoleSchema, 
+  userSchema,
+  accountActivationCodeSchema,
+  passwordValidationSchema, 
+} from "../schemas/user.schema.js";
 
 // Generate Access & Refresh Token
 const generateAccessAndRefreshToken = async (userId: string) => {
@@ -59,7 +74,7 @@ const registerUser = asyncHandler(async (req: IAuthenticatedRequest, res: Respon
 
       isUserExisted.password = validatedData.password;
       isUserExisted.verifyCode = verifyCode;
-      isUserExisted.codeExpiry = codeExpiry;
+      isUserExisted.verifyCodeExpiry = codeExpiry;
       isUserExisted.isVerified = false;
       isUserExisted.refreshToken = refreshToken;
 
@@ -78,7 +93,7 @@ const registerUser = asyncHandler(async (req: IAuthenticatedRequest, res: Respon
       email: validatedData.email,
       password: validatedData.password,
       verifyCode,
-      codeExpiry,
+      verifyCodeExpiry: codeExpiry,
       isVerified: false,
     });
     
@@ -103,7 +118,7 @@ const registerUser = asyncHandler(async (req: IAuthenticatedRequest, res: Respon
     isUserExisted = saveNewUser;
   }
 
-  const isSendOtpEmail = await sendOTPEmail(validatedData.userName, validatedData.email, verifyCode);
+  const isSendOtpEmail = await sendEmailVerificationCode(validatedData.userName, validatedData.email, verifyCode);
 
   if (!isSendOtpEmail) {
     throw new ApiError(500, "Failed to send OTP, please try again later");
@@ -132,12 +147,651 @@ const registerUser = asyncHandler(async (req: IAuthenticatedRequest, res: Respon
         },
         token: {
           accessToken: responseAccessToken,
+          refreshToken: responseRefreshToken,
         }
       }
     )
   );
 });
 
+// Verify user account with OTP
+const verifyUserAccount = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const validatedData = verifyOtpSchema.parse(req.body);
+
+  const user = await UserModel.findById(req.user._id);
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  if (user.isVerified) {
+    return res.status(200)
+    .json(
+      new ApiResponse(
+        200,
+        "User account is already verified",
+        {
+          user: {
+            _id: user._id,
+            userName: user.userName,
+            email: user.email,
+            isVerified: user.isVerified,
+          },
+        }
+      )
+    );
+  }
+
+  if (user.verifyCode !== validatedData.verifyCode) {
+    throw new ApiError(400, "Invalid OTP code");
+  }
+
+  if (user.verifyCodeExpiry && user.verifyCodeExpiry < new Date()) {
+    throw new ApiError(400, "OTP code has expired");
+  }
+
+  user.verifyCode = null;
+  user.verifyCodeExpiry = null;
+  user.isVerified = true;
+
+  const saveVerifiedUser = await user.save();
+
+  if (!saveVerifiedUser) {
+    throw new ApiError(500, "Failed to verify user account");
+  }
+
+  return res.status(200)
+  .json(
+    new ApiResponse(
+      200,
+      "User account verified successfully",
+      {
+        user: {
+          _id: saveVerifiedUser._id,
+          userName: saveVerifiedUser.userName,
+          email: saveVerifiedUser.email,
+          isVerified: saveVerifiedUser.isVerified,
+          createdAt: saveVerifiedUser.createdAt,
+          updatedAt: saveVerifiedUser.updatedAt,
+        },
+      }
+    )
+  );
+});
+
+// Resend OTP
+const resendOTP = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const user = await UserModel.findById(req.user._id);
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  // If user is already verified
+  if (user.isVerified) {
+    throw new ApiError(400, "User account is already verified");
+  }
+
+  // Generate 6-digits OTP code
+  const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+  // OTP code valid only for 10 minutes
+  const codeExpiry = new Date();
+  codeExpiry.setMinutes(codeExpiry.getMinutes() + 10);
+
+  user.verifyCode = verifyCode;
+  user.verifyCodeExpiry = codeExpiry;
+
+  const saveUser = await user.save();
+
+  if (!saveUser) {
+    throw new ApiError(500, "Failed to generate verification code");
+  }
+
+  const isSendOtpEmail = await sendEmailVerificationCode(user.userName, user.email, verifyCode);
+
+  if (!isSendOtpEmail) {
+    throw new ApiError(500, "Failed to send OTP, please try again later");
+  }
+
+  return res.status(200)
+  .json(
+    new ApiResponse(
+      200,
+      "OTP resent successfully",
+      {
+        user: {
+          _id: saveUser._id,
+          userName: saveUser.userName,
+          email: saveUser.email,
+          isVerified: saveUser.isVerified,
+        },
+      }
+    )
+  );
+});
+
+// Login user
+const loginUser = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const validatedData = loginSchema.parse(req.body);
+
+  const user = await UserModel.findOne({
+    $or: [
+      { userName: validatedData.identifier },
+      { email: validatedData.identifier },
+    ],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const isCorrectPassword = await user.comparePassword(validatedData.password);
+
+  if (!isCorrectPassword) {
+    throw new ApiError(401, "Incorrect password");
+  }
+
+  if (user.isBanned) {
+    throw new ApiError(403, "Your account has been banned due to violations of community guidelines.");
+  }
+
+  if (!user.isActive) {
+    throw new ApiError(
+      403, 
+      "Your account is currently deactivated. Please reactivate to continue",
+      [
+        {
+          "requireAccountActivation": true,
+        }
+      ]
+      
+    );
+  }
+
+  let { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id);
+
+  user.refreshToken = refreshToken;
+
+  const saveUser = await user.save();
+
+  if (!saveUser) {
+    throw new ApiError(500, "Failed to login user");
+  }
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  }
+
+  return res.status(200)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", refreshToken, options)
+  .json(
+    new ApiResponse(
+      200,
+      "User logged in successfully",
+      {
+        user: {
+          _id: saveUser._id,
+          userName: saveUser.userName,
+          email: saveUser.email,
+          isVerified: saveUser.isVerified,
+          createdAt: saveUser.createdAt,
+          updatedAt: saveUser.updatedAt,
+        },
+        token: {
+          accessToken: accessToken,
+          refreshToken,
+        },
+      }
+    )
+  );
+});
+
+// Logout user
+const logoutUser = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const user = await UserModel.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset: {
+        refreshToken: 1,
+      }
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  }
+
+  return res.status(200)
+  .clearCookie("accessToken", options)
+  .clearCookie("refreshToken", options)
+  .json(
+    new ApiResponse(
+      200,
+      "User logged out successfully",
+    )
+  );
+});
+
+// Update password
+const updatePassword = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const validatedData = updatePasswordSchema.parse(req.body);
+
+  const user = await UserModel.findById(req.user?._id);
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const isCorrectPassword = await user.comparePassword(validatedData.currentPassword);
+
+  if (!isCorrectPassword) {
+    throw new ApiError(400, "Incorrect current password");
+  }
+
+  user.password = validatedData.newPassword;
+
+  const saveUser = await user.save({
+    validateBeforeSave: false,
+  });
+
+  if (!saveUser) {
+    throw new ApiError(500, "Failed to update password");
+  }
+
+  return res.status(200)
+  .json(
+    new ApiResponse(
+      200,
+      "Password updated successfully",
+    )
+  );
+});
+
+// Refresh token
+const updateToken = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+  const validatedData = refreshTokenSchema.parse(incomingRefreshToken); 
+
+  try {
+    const decodedToken = await jwt.verify(
+      validatedData.refreshToken,
+      config.jwtRefreshTokenSecret as any,
+    ) as JwtPayload;
+
+    const user = await UserModel.findById(decodedToken?._id);
+
+    if (!user) {
+      throw new ApiError(401, "Invalid refresh token");
+    }
+
+    if (user.isBanned) {
+      throw new ApiError(403, "User account is temporarily suspended");
+    }
+
+    if (incomingRefreshToken !== user.refreshToken) {
+      throw new ApiError(401, "Token has expired");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id);
+
+    user.refreshToken = refreshToken;
+
+    const saveUser = await user.save();
+
+    if (!saveUser) {
+      throw new ApiError(500, "Failed to refresh token");
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    }
+
+    return res.status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        "Token refreshed successfully",
+        {
+          token: {
+            accessToken: accessToken,
+            refreshToken: refreshToken
+          },
+        }
+      )
+    );
+  } catch (error) {
+    throw new ApiError(
+      401,
+      "Some thing went wrong, while trying to refresh the token",
+    )
+  }
+});
+
+// Update user role
+const updateUserRole = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const { id, role } = userRoleSchema.parse(req.body);
+
+  const user = await UserModel.findByIdAndUpdate(
+    id,
+    { role },
+    { new: true }
+  ).select("-password -refreshToken");
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  return res.status(200)
+  .json(
+    new ApiResponse(
+      200,
+      "User role updated successfully",
+      {
+        user,
+      }
+    )
+  );
+});
+
+// Get current user
+const getCurrentUser = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const user = await UserModel.findById(req.user._id)
+  .select("-password -refreshToken");
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  return res.status(200)
+  .json(
+    new ApiResponse(
+      200,
+      "User retrieved successfully",
+      {
+        user,
+      }
+    )
+  );
+});
+
+// Update user account details
+const updateUserAccountDetails = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const validatedData = updateUserProfileSchema.parse(req.body);
+
+  const isUserNameExist = await UserModel.findOne({ userName: validatedData.userName });
+
+  if (isUserNameExist && isUserNameExist._id.toString() !== req.user?._id.toString()) {
+    throw new ApiError(400, "Username already exists");
+  }
+
+  const user = await UserModel.findByIdAndUpdate(
+    req.user?._id,
+    validatedData,
+    { new: true }
+  ).select("-password -refreshToken");
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  return res.status(200)
+  .json(
+    new ApiResponse(
+      200,
+      "User account details updated successfully",
+      {
+        user,
+      }
+    )
+  );
+});
+
+// Get user by id
+const getUserById = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const validatedData = getUserByIdSchema.parse(req?.params);
+
+  const user = await UserModel.findById(validatedData.id)
+  .select("-password -refreshToken");
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  return res.status(200)
+  .json(
+    new ApiResponse(
+      200,
+      "User retrieved successfully",
+      {
+        user,
+      }
+    )
+  );
+});
+
+// Get all users
+const getAllUsers = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const validatedData = getAllUsersSchema.parse(req.query);
+
+  const page = validatedData.page;
+  const limit = validatedData.limit;
+
+  const skip = (page - 1) * limit;
+
+  const users = await UserModel.find()
+  .select("-password -refreshToken")
+  .skip(skip)
+  .limit(limit);
+
+  const totalUsers = await UserModel.countDocuments();
+
+  return res.status(200)
+  .json(
+    new ApiResponse(
+      200,
+      "Users retrieved successfully",
+      {
+        users,
+        totalUsers,
+        currentPage: page,
+        limit: limit,
+        totalPages: Math.ceil(totalUsers / limit),
+      }
+    )
+  );
+});
+
+// Get account activation code
+const getAccountActivationCode = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const validatedData = emailValidationSchema.parse(req.body);
+
+  const user = await UserModel.findOne({email: validatedData.email});
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  if (user.isActive) {
+    throw new ApiError(400, "User account is already active");
+  }
+
+  // Generate 4-digits OTP code
+  const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
+
+  // OTP code valid only for 10 minutes
+  const codeExpiry = new Date();
+  codeExpiry.setMinutes(codeExpiry.getMinutes() + 10);
+
+  user.accountActivationCode = activationCode;
+  user.activationCodeExpiry = codeExpiry;
+
+  const saveUser = await user.save();
+
+  if (!saveUser) {
+    throw new ApiError(500, "Failed to generate activation code");
+  }
+
+  const isSendActivationCode = await sendAccountActivationCode(user.userName, user.email, activationCode);
+
+  if (!isSendActivationCode) {
+    throw new ApiError(500, "Failed to send activation code");
+  }
+
+  return res.status(200)
+  .json(
+    new ApiResponse(
+      200,
+      "Account activation code has been sent to your email address"
+    )
+  );
+});
+
+// Verify account activation code
+const verifyAccountActivationCode = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const validatedData = accountActivationCodeSchema.parse(req.body);
+
+  const user = await UserModel.findOne({ email: validatedData.email });
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  if (user.isActive) {
+    throw new ApiError(400, "User account is already active");
+  }
+
+  if (validatedData.code !== user.accountActivationCode) {
+    throw new ApiError(400, "Invalid account activation code");
+  }
+
+  if (user.activationCodeExpiry && user.activationCodeExpiry < new Date()) {
+    throw new ApiError(400, "Activation code has expired");
+  }
+
+  user.isActive = true;
+  user.accountActivationCode = null;
+  user.activationCodeExpiry = null;
+
+  const saveUser = await user.save();
+
+  if (!saveUser) {
+    throw new ApiError(500, "Failed to verify account activation code");
+  }
+
+  return res.status(200)
+  .json(
+    new ApiResponse(
+      200,
+      "Account activated successfully, now you can login",
+    )
+  );
+});
+
+// Toggle account activity
+const deactivateAccount = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const user = await UserModel.findByIdAndUpdate(
+    req.user?._id,
+    { 
+      isActive: false,
+      $unset: {
+        refreshToken: 1,
+      }
+    },
+    { new: true }
+  );
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const saveUser = await user.save();
+
+  if (!saveUser) {
+    throw new ApiError(500, `Failed to deactivate account`);
+  }
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  }
+
+  return res.status(200)
+  .clearCookie("accessToken", options)
+  .clearCookie("refreshToken", options)
+  .json(
+    new ApiResponse(
+      200,
+      "Account deactivated successfully & you have logged out",
+    )
+  );
+});
+
+// Delete account
+const deleteAccount = asyncHandler(async (req: IAuthenticatedRequest, res: Response) => {
+  const validatedData = passwordValidationSchema.parse(req.body);
+
+  const user = await UserModel.findById(req.user?._id);
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const isCorrectPassword = await user.comparePassword(validatedData.password);
+
+  if (!isCorrectPassword) {
+    throw new ApiError(401, "Incorrect password");
+  }
+
+  user.deletedAt = new Date();
+
+  const saveUser = await user.save();
+
+  // TODO: Delete user related data in the database
+
+  if (!saveUser) {
+    throw new ApiError(500, "Failed to delete account");
+  }
+
+  return res.status(200)
+  .json(
+    new ApiResponse(
+      200,
+      "Account deleted successfully",
+    )
+  );
+});
+
+
 export {
   registerUser,
+  verifyUserAccount,
+  resendOTP,
+  loginUser,
+  logoutUser,
+  updatePassword,
+  updateToken,
+  updateUserRole,
+  getCurrentUser,
+  updateUserAccountDetails,
+  getUserById,
+  getAllUsers,
+  getAccountActivationCode,
+  verifyAccountActivationCode,
+  deactivateAccount,
+  deleteAccount,
 }
--- backend/src/emails/sendOtpEmail.ts
+++ backend/src/emails/sendOtpEmail.ts
@@ -1,7 +1,8 @@
 import nodemailer from "nodemailer";
 import { config } from "../config/config.js";
 
-const sendOTPEmail = async ( userName: string, email: string, otp: string): Promise<boolean> => {
+// Email verification
+const sendEmailVerificationCode = async ( userName: string, email: string, otp: string): Promise<boolean> => {
   try {
     const transporter = nodemailer.createTransport({
       service: "gmail",
@@ -67,4 +68,75 @@ const sendOTPEmail = async ( userName: string, email: string, otp: string): Prom
   }
 };
 
-export { sendOTPEmail };
+// Account activation
+const sendAccountActivationCode = async ( userName: string, email: string, otp: string): Promise<boolean> => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      secure: true,
+      port: 465,
+      auth: { 
+        user: config.otpEmailAddress, 
+        pass: config.otpEmailPassword, 
+      },
+    });
+  
+    const emailHtml = `
+      <html>
+        <head>
+          <style>
+            body { 
+              font-family: Arial, sans-serif; 
+              background-color: #f4f4f4; }
+            .container { 
+              max-width: 600px; 
+              margin: 20px auto; 
+              background: #ffffff; 
+              padding: 20px; 
+              border-radius: 8px; 
+              text-align: left; }
+            .otp { 
+              font-size: 24px; 
+              font-weight: bold; 
+              color: #007bff; 
+              background: #e9ecef; 
+              padding: 10px 20px; 
+              border-radius: 6px; 
+              display: inline-block; }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h2>Activate Your Account</h2>
+            <p>Hello <strong>${userName}</strong>,</p>
+            <p>We noticed that you requested to activate your account on LinkSphere.</p>
+            <p>Your activation code is:</p>
+            <div class="otp">${otp}</div>
+            <p>This code is valid for next <strong>10 minutes</strong>. Please do not share it with anyone.</p>
+            <p>If you did not request this, please ignore this email.</p>
+            <p class="best-regards">Best Regards,</p>
+            <p class="link-sphere"><strong>LinkSphere</strong></p>
+            <p style="font-size: 12px; color: #888;">&copy; 2025 LinkSphere. All Rights Reserved.</p>
+          </div>
+        </body>
+      </html>
+    `;
+  
+    const response = await transporter.sendMail({
+      from: `LinkSphere <linksphere@socialmedia.in>`,
+      to: email,
+      subject: "Activate Your Account",
+      html: emailHtml,
+    });
+
+    return true;
+  } catch (error) {
+    console.error(`Error while send otp: ${error}`);
+    return false;
+  }
+};
+
+export { 
+  sendEmailVerificationCode,
+  sendAccountActivationCode, 
+};
