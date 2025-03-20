8c5916fabc5921350ddcb7bc131a0546de682eab
azaadmottan <azaadmottan2022@gmail.com>
2025-03-21 00:35:57 +0530

    Add 'Blocked' fields in the follow interface

--- backend/src/interfaces/follow.interface.ts
+++ backend/src/interfaces/follow.interface.ts
@@ -3,7 +3,7 @@ import { Document, Types } from "mongoose";
 export interface IFollow extends Document {
   follower: Types.ObjectId; // User who follows
   following: Types.ObjectId; // User who is being followed
-  status: "Pending" | "Accepted"; // For handling follow requests
+  status: "Pending" | "Accepted" | "Blocked"; // For handling follow requests
   createdAt: Date;
   updatedAt: Date;
 }
