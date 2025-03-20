348d4a9dbcffa64b77a6bd61d8b2ca7feea530a8
azaadmottan <azaadmottan2022@gmail.com>
2025-03-21 00:38:06 +0530

    Setup routes for follow: sendFollowRequest, acceptFollowRequest, cancelFollowRequest, rejectFollowRequest, unfollowUser, removeFollower, getUserSocialProfile, getUserFollowerList, getUserFollowingList, getFollowerRequests, getFollowingRequests, blockUser, unblockUser, getBlockedUsers.

---
+++ backend/src/routes/follow.routes.ts
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import { authenticateUser } from "../middlewares/auth.middleware.js";
+import { 
+  acceptFollowRequest,
+  blockUser,
+  cancelFollowRequest,
+  getBlockedUsers,
+  getFollowerRequests,
+  getFollowingRequests,
+  getUserFollowerList,
+  getUserFollowingList,
+  getUserSocialProfile,
+  rejectFollowRequest,
+  removeFollower,
+  sendFollowRequest,
+  unblockUser,
+  unfollowUser,
+} from "../controllers/follow.controller.js";
+
+const router = Router();
+
+// Follow routes
+
+router.route("/send-follow-request").post(authenticateUser, sendFollowRequest);
+router.route("/accept-follow-request").post(authenticateUser, acceptFollowRequest);
+router.route("/cancel-follow-request").delete(authenticateUser, cancelFollowRequest);
+router.route("/reject-follow-request").delete(authenticateUser, rejectFollowRequest);
+router.route("/unfollow-user").delete(authenticateUser, unfollowUser);
+router.route("/remove-follower").delete(authenticateUser, removeFollower);
+router.route("/get-social-profile/:id").get(authenticateUser, getUserSocialProfile);
+router.route("/get-user-follower/:id").get(authenticateUser, getUserFollowerList);
+router.route("/get-user-following/:id").get(authenticateUser, getUserFollowingList);
+router.route("/get-follower-requests").get(authenticateUser, getFollowerRequests);
+router.route("/get-following-requests").get(authenticateUser, getFollowingRequests);
+router.route("/block-user").delete(authenticateUser, blockUser);
+router.route("/unblock-user").patch(authenticateUser, unblockUser);
+router.route("/get-blocked-users").get(authenticateUser, getBlockedUsers);
+
+export default router;
