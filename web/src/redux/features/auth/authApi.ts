8d92db2a44fd37638bbce01a34611cd6310b23c7
azaadmottan <azaadmottan2022@gmail.com>
2025-04-13 20:14:38 +0530

    Setup RTK authApi's

---
+++ web/src/redux/features/auth/authApi.ts
@@ -0,0 +1,16 @@
+import { API_ENDPOINT } from "@/constants/apiEndpoint";
+import { baseApi } from "@/redux/store/baseApi";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    registerUser: builder.mutation({
+      query: (body) => ({
+        url: API_ENDPOINT.AUTH.REGISTER,
+        method: "POST",
+        data: body,
+      })
+    })
+  }),
+});
+
+export const { useRegisterUserMutation } = authApi;
